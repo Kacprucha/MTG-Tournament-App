65928d1b94decce5a6ca83db855d304d335c9ec3
Kacper Karaś <karas.kacper@gmail.com>
2025-08-19 02:24:39 +0200

    Changing pading in button

--- frontend/app/components/Button.tsx
+++ frontend/app/components/Button.tsx
@@ -7,7 +7,7 @@ export default function Button({ text, onClick }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="px-4 py-1 border border-cyan-400 text-white rounded-full text-sm font-bold hover:bg-cyan-400 hover:text-black transition"
+      className="px-4 py-2 border border-cyan-400 text-white rounded-full text-sm font-bold hover:bg-cyan-400 hover:text-black transition"
     >
       {text}
     </button>
