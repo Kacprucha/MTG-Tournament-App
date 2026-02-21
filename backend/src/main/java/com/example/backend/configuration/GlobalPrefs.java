16baa88fd03ed1164a9d7a925d111a62bc58b14a
Kacper Karaś <karas.kacper@gmail.com>
2026-02-21 18:18:26 +0100

    Adding static prefs file

---
+++ backend/src/main/java/com/example/backend/configuration/GlobalPrefs.java
@@ -0,0 +1,12 @@
+package com.example.backend.configuration;
+
+public class GlobalPrefs 
+{
+    public static final int POINTS_FOR_WIN = 1;
+    
+    private GlobalPrefs() 
+    {
+        // Prevent instantiation
+    }
+
+}
